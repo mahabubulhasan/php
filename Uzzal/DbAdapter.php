763b9a240c30521d135e327f8bd67bdc1491aca2
A N M Mababubul Hasan <uzzal.me@gmail.com>
2013-05-02 09:49:44 +0600

    use buffered query attribute added. This is important when using SP.

--- Uzzal/DbAdapter.php
+++ Uzzal/DbAdapter.php
@@ -13,6 +13,10 @@ class DbAdapter extends \PDO{
         parent::__construct($config['dsn'],$config['username'],$config['password']);
     }
 
+    public function useBufferedQuery(){
+        $this->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, TRUE);
+    }
+
     public static function getInstance($config){
         if(!self::$_me){
             self::$_me = new DbAdapter($config);
@@ -34,8 +38,8 @@ class DbAdapter extends \PDO{
             $stat = $this->_prepareExecute($sql, $args);
         }else{
             $stat = $this->query($sql);
-        }        
-        
+        }
+
         return $stat->fetchAll($this->getFetchMode());
     }
 
@@ -45,7 +49,7 @@ class DbAdapter extends \PDO{
         }else{
             $stat = $this->query($sql);
         }
-        
+
         return $stat->fetch($this->getFetchMode());
     }
 
@@ -56,51 +60,51 @@ class DbAdapter extends \PDO{
         }else{
             $stat->execute(array($args));
         }
-        
+
         return $stat;
     }
-    
+
     private function _execute($sql){
-        $stat = $this->prepare($sql);        
+        $stat = $this->prepare($sql);
         $stat->execute($args);
-                
+
         return $stat;
     }
-        
+
     public function insert($table, array $data){
         foreach($data as $d){
             $w[] = '?';
             $args[] = $d;
         }
-        
+
         $cols = array_keys($data);
         array_walk($cols, function(&$key){$key = '`'.$key.'`';});
-        
+
         $cols = implode(',',$cols);
         $vals = implode(',',$w);
-        
+
         $sql = "INSERT INTO `{$table}` ({$cols}) VALUES ({$vals})";
         $stat = $this->_prepareExecute($sql, $args);
-        
+
         return $stat->rowCount();
     }
-    
+
     public function update($table, array $data, $cond=''){
-        foreach($data as $k=>$v){            
+        foreach($data as $k=>$v){
             if(is_numeric($v)){
                 $args[] = "`{$k}`={$v}";
             }else{
                 $args[] = "`{$k}`='{$v}'";
             }
         }
-        
+
         $values = implode(',', $args);
         if($cond){
             $cond = ' WHERE '.$cond;
         }
-        
+
         $sql = "UPDATE `{$table}` SET {$values}".$cond;
-        
+
         $stat = $this->_execute($sql);
         return $stat->rowCount();
     }
